c1904008492256f02f84738f985b0f3bfd7b53e3
Maksim Sunoplya <969715@mail.ru>
2022-04-02 14:44:26 +0400

    Added findAllByModel method

--- src/main/java/com/simbirsoft/cars/repository/CarRepository.java
+++ src/main/java/com/simbirsoft/cars/repository/CarRepository.java
@@ -3,8 +3,11 @@ package com.simbirsoft.cars.repository;
 import com.simbirsoft.cars.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
     Optional<Car> findByModel(String model);
+
+    List<Car> findAllByModel(String model);
 }
