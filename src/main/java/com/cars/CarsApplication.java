d44206b50dc75104b528fadfc6337c6eed66e176
Maksim Sunoplya <maksim.sunoplya@nordclan.com>
2022-04-07 12:31:21 +0400

    Change2

--- src/main/java/com/cars/CarsApplication.java
+++ src/main/java/com/cars/CarsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarsApplication {
 
+    //change2
     public static void main(String[] args) {
         SpringApplication.run(CarsApplication.class, args);
     }
