35d3cdaea81709527de7db03b229a1da01e3d0a7
Maksim Sunoplya <969715@mail.ru>
2022-04-10 15:37:44 +0400

    Added lock wait, notifyAll

--- src/test/java/com/cars/multithreading/LockWait.java
+++ src/test/java/com/cars/multithreading/LockWait.java
@@ -18,21 +18,28 @@ public class LockWait {
                 try {
                     log.info("Waiting...");
                     lock.wait();
-                    TimeUnit.SECONDS.sleep(3);
+                    TimeUnit.SECONDS.sleep(1);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                log.info("Thread1 finished");
+                log.info("Thread finished");
             }
         };
 
         Thread thread1 = new Thread(task);
         thread1.start();
+        Thread thread2 = new Thread(task);
+        thread2.start();
+        Thread thread3 = new Thread(task);
+        thread3.start();
 
         TimeUnit.SECONDS.sleep(3);
         log.info("Main get lock and notify");
         synchronized (lock) {
             lock.notify();
+            lock.notify();
+            lock.notify();
+
         }
         log.info("Main finished");
     }
