c039ee26f726ee6c4ae650197aad5db27c6c2828
Maksim Sunoplya <969715@mail.ru>
2022-04-10 13:50:52 +0400

    Added thread join test

---
+++ src/test/java/com/cars/multithreading/ThreadJoin.java
@@ -0,0 +1,25 @@
+package com.cars.multithreading;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.util.concurrent.TimeUnit;
+
+@Log4j2
+public class ThreadJoin {
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task = () -> {
+            log.info("Task");
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Thread thread = new Thread(task);
+        log.info("Thread start");
+        thread.start();
+        thread.join();
+        log.info("Finished");
+    }
+}
