97dd6f9b8f62f4e17c20c02f8ba1c3a31a30565d
Maksim Sunoplya <969715@mail.ru>
2022-04-10 14:08:51 +0400

    Refactored

--- src/test/java/com/cars/multithreading/ThreadInterrupt.java
+++ src/test/java/com/cars/multithreading/ThreadInterrupt.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Thread.interrupt() - прерывание потока или
+ */
 @Slf4j
 public class ThreadInterrupt {
     public static void main(String[] args) throws InterruptedException {
--- src/test/java/com/cars/multithreading/ThreadJoin.java
+++ src/test/java/com/cars/multithreading/ThreadJoin.java
@@ -4,6 +4,9 @@ import lombok.extern.log4j.Log4j2;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Thread.join() — ожидание завершения другого потока
+ */
 @Log4j2
 public class ThreadJoin {
     public static void main(String[] args) throws InterruptedException {
