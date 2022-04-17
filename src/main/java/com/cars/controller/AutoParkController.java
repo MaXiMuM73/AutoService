b947ab24fecc9f25eb488cd9f0416cb1d5364822
Maksim Sunoplya <969715@mail.ru>
2022-04-17 21:07:32 +0400

    Added CRUD

--- src/main/java/com/cars/controller/AutoParkController.java
+++ src/main/java/com/cars/controller/AutoParkController.java
@@ -1,9 +1,11 @@
 package com.cars.controller;
 
+import com.cars.config.constants.Urls;
 import com.cars.dto.AutoParkCreateDto;
+import com.cars.dto.AutoParkDto;
+import com.cars.dto.AutoParkUpdateDto;
 import com.cars.dto.CarDto;
 import com.cars.service.AutoParkService;
-import com.cars.config.constants.Urls;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,17 +31,27 @@ public class AutoParkController {
 
     @Operation(summary = "Добавить автопарк")
     @PostMapping(Urls.AutoPark.FULL)
-    public ResponseEntity<AutoParkCreateDto> createAutoPark(@RequestBody AutoParkCreateDto autoParkCreateDto) {
+    public ResponseEntity<AutoParkDto> createAutoPark(@RequestBody AutoParkCreateDto autoParkCreateDto) {
         return ResponseEntity.ok(autoParkService.create(autoParkCreateDto));
+    }
+
+    @Operation(summary = "Обновить автопарк")
+    @PutMapping(Urls.AutoPark.FULL)
+    public ResponseEntity<AutoParkDto> update(@RequestBody AutoParkUpdateDto autoParkUpdateDto) {
+        return ResponseEntity.ok(autoParkService.update(autoParkUpdateDto));
+    }
+
+    @Operation(summary = "Удалить автопарк")
+    @DeleteMapping(Urls.AutoPark.Id.FULL)
+    public ResponseEntity<AutoParkDto> delete(@PathVariable Long autoParkId) {
+        return ResponseEntity.ok(autoParkService.delete(autoParkId));
     }
 
     @Operation(summary = "Найти все автопарки")
     @GetMapping(value = Urls.AutoPark.FULL)
-    public ResponseEntity<List<AutoParkCreateDto>> findAll() {
+    public ResponseEntity<List<AutoParkDto>> findAll() {
         return ResponseEntity.ok(autoParkService.findAll());
     }
-
-
 
     @PostMapping(Urls.AutoPark.Id.Car.CarId.FULL)
     public ResponseEntity<List<CarDto>> addCar(@PathVariable Long autoParkId,
