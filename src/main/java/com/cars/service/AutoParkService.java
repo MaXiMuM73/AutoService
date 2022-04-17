e7dc1bd17c4d65f615c5daa33ea2e67fde45667f
Maksim Sunoplya <969715@mail.ru>
2022-04-17 21:08:24 +0400

    Added CRUD

--- src/main/java/com/cars/service/AutoParkService.java
+++ src/main/java/com/cars/service/AutoParkService.java
@@ -1,16 +1,22 @@
 package com.cars.service;
 
 import com.cars.dto.AutoParkCreateDto;
+import com.cars.dto.AutoParkDto;
+import com.cars.dto.AutoParkUpdateDto;
 import com.cars.dto.CarDto;
 
 import java.util.List;
 
 public interface AutoParkService {
-    AutoParkCreateDto create(AutoParkCreateDto autoParkCreateDto);
+    AutoParkDto create(AutoParkCreateDto autoParkCreateDto);
+
+    AutoParkDto update(AutoParkUpdateDto autoParkUpdateDto);
+
+    AutoParkDto delete(Long id);
 
     List<CarDto> addCar(Long autoParkId, Long carId);
 
     void deleteCar(Long autoParkId, Long carId);
 
-    List<AutoParkCreateDto> findAll();
+    List<AutoParkDto> findAll();
 }
--- src/main/java/com/cars/service/impl/AutoParkServiceImpl.java
+++ src/main/java/com/cars/service/impl/AutoParkServiceImpl.java
@@ -1,40 +1,80 @@
 package com.cars.service.impl;
 
 import com.cars.dto.AutoParkCreateDto;
+import com.cars.dto.AutoParkDto;
+import com.cars.dto.AutoParkUpdateDto;
 import com.cars.dto.CarDto;
 import com.cars.entity.AutoPark;
 import com.cars.entity.Car;
 import com.cars.exception.AutoParkIdNotFoundException;
 import com.cars.repository.AutoParkRepository;
-import com.cars.repository.CarRepository;
 import com.cars.service.AutoParkService;
+import com.cars.service.CarService;
 import com.cars.service.mapper.AutoParkMapper;
 import com.cars.service.mapper.CarMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
+@Log4j2
 @Service
+@AllArgsConstructor
 public class AutoParkServiceImpl implements AutoParkService {
 
     private final AutoParkRepository autoParkRepository;
+    private final CarService carService;
+
     private final AutoParkMapper autoParkMapper;
-    private final CarRepository carRepository;
     private final CarMapper carMapper;
 
-    public AutoParkServiceImpl(AutoParkRepository autoParkRepository,
-                               AutoParkMapper autoParkMapper, CarRepository carRepository,
-                               CarMapper carMapper) {
-        this.autoParkRepository = autoParkRepository;
-        this.autoParkMapper = autoParkMapper;
-        this.carRepository = carRepository;
-        this.carMapper = carMapper;
+    @Override
+    public AutoParkDto create(AutoParkCreateDto autoParkCreateDto) {
+        AutoPark autoPark = autoParkRepository
+                .save(autoParkMapper.toEntity(autoParkCreateDto));
+
+        log.info("Autopark created {}", autoPark);
+
+        return autoParkMapper.toDto(autoPark);
     }
 
     @Override
-    public List<AutoParkCreateDto> findAll() {
+    public AutoParkDto update(AutoParkUpdateDto autoParkUpdateDto) {
+        AutoPark autopark = findById(autoParkUpdateDto.getId());
+        autopark.setName(autoParkUpdateDto.getName());
+
+        return autoParkMapper.toDto(autopark);
+    }
+
+    @Override
+    public AutoParkDto delete(Long id) {
+        AutoPark autoPark = findById(id);
+        autoParkRepository.delete(autoPark);
+
+        log.info("Autopark with id {} deleted", id);
+
+        return autoParkMapper.toDto(autoPark);
+    }
+
+    @Override
+    @Transactional
+    public List<CarDto> addCar(Long autoParkId, Long carId) {
+        AutoPark autoPark = findById(autoParkId);
+        Car car = carService.findById(carId);
+        if (autoPark.getCars().add(car)) {
+            car.setAutoPark(autoPark);
+        }
+
+        autoParkRepository.save(autoPark);
+
+        return autoPark.getCars().stream().map(carMapper::toDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<AutoParkDto> findAll() {
         return autoParkRepository.findAll()
                 .stream()
                 .map(autoParkMapper::toDto)
@@ -42,28 +82,11 @@ public class AutoParkServiceImpl implements AutoParkService {
     }
 
     @Override
-    public AutoParkCreateDto create(AutoParkCreateDto autoParkCreateDto) {
-        AutoPark autoPark = autoParkRepository.save(autoParkMapper.toEntity(autoParkCreateDto));
-        return autoParkMapper.toDto(autoPark);
-    }
-
-    @Override
-    public List<CarDto> addCar(Long autoParkId, Long carId) {
-        AutoPark autoPark = findById(autoParkId);
-        Car car = carRepository.findById(carId).get();
-        car.setAutoPark(autoPark);
-        Set<Car> cars = autoPark.getCars();
-        cars.add(car);
-        autoParkRepository.save(autoPark);
-        return autoPark.getCars().stream().map(carMapper::toDto).collect(Collectors.toList());
-    }
-
-    @Override
     public void deleteCar(Long autoParkId, Long carId) {
-        AutoPark autoPark = autoParkRepository.findById(autoParkId).get();
-        Car car = carRepository.findById(carId).get();
-        Set<Car> cars = autoPark.getCars();
-        cars.remove(car);
+        AutoPark autoPark = findById(autoParkId);
+        Car car = carService.findById(carId);
+        autoPark.getCars().remove(car);
+
         autoParkRepository.save(autoPark);
     }
 
