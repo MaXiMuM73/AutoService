1bc1f239095aedbc1e374406ac46c704706f9ed5
Maksim Sunoplya <969715@mail.ru>
2022-04-17 21:08:01 +0400

    Refactoring

--- src/main/java/com/cars/service/mapper/AutoParkMapper.java
+++ src/main/java/com/cars/service/mapper/AutoParkMapper.java
@@ -1,17 +1,28 @@
 package com.cars.service.mapper;
 
 import com.cars.dto.AutoParkCreateDto;
+import com.cars.dto.AutoParkDto;
 import com.cars.entity.AutoPark;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
 @Component
+@AllArgsConstructor
 public class AutoParkMapper {
 
-    public AutoParkCreateDto toDto(AutoPark autoPark) {
-        AutoParkCreateDto autoParkCreateDto = new AutoParkCreateDto();
-        autoParkCreateDto.setId(autoPark.getId());
-        autoParkCreateDto.setName(autoPark.getName());
-        return autoParkCreateDto;
+    private final CarMapper carMapper;
+
+    public AutoParkDto toDto(AutoPark autoPark) {
+        AutoParkDto autoParkDto = new AutoParkDto();
+        autoParkDto.setId(autoPark.getId());
+        autoParkDto.setName(autoPark.getName());
+        autoParkDto.setCars(autoPark.getCars()
+                .stream()
+                .map(carMapper::toDto)
+                .collect(Collectors.toList()));
+        return autoParkDto;
     }
 
     public AutoPark toEntity(AutoParkCreateDto autoParkCreateDto) {
