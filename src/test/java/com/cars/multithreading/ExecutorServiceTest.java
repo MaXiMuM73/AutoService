961d0a0d18360369c09dc8deeee43f8958ee6126
Maksim Sunoplya <969715@mail.ru>
2022-04-24 11:58:39 +0400

    Executor service test

--- src/test/java/com/cars/multithreading/ExecutorServiceTest.java
+++ src/test/java/com/cars/multithreading/ExecutorServiceTest.java
@@ -2,18 +2,26 @@ package com.cars.multithreading;
 
 import lombok.extern.log4j.Log4j2;
 
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Log4j2
 public class ExecutorServiceTest {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         Callable<String> task = () -> {
-            log.info("working");
+            log.info("working callable");
             return Thread.currentThread().getName();
+        };
+        Runnable taskRunnable = () -> {
+            log.info("working runnable");
         };
         ExecutorService executorService = Executors.newFixedThreadPool(2);
 
         for (int i = 0; i < 10; i++) {
+            executorService.submit(taskRunnable);
             Future<String> result = executorService.submit(task);
             result.get();
         }
