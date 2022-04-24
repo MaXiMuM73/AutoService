a148034e9d00c95107e08f95f3c375a75882cf37
Maksim Sunoplya <969715@mail.ru>
2022-04-24 14:25:25 +0400

    Save increment

---
+++ src/test/java/com/cars/multithreading/saveincrement/SaveIncrementTest.java
@@ -0,0 +1,36 @@
+package com.cars.multithreading.saveincrement;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Log4j2
+public class SaveIncrementTest {
+    private static final AtomicLong count = new AtomicLong(0);
+    private static final Lock lock = new ReentrantLock();
+    private static final Semaphore semaphore = new Semaphore(5);
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable incrementTask = () -> {
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info(Thread.currentThread().getName() + " working");
+            for (int i = 0; i < 1000000; i++) {
+                count.incrementAndGet();
+            }
+            log.info(Thread.currentThread().getName() + ": {}", count);
+            semaphore.release();
+        };
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(incrementTask).start();
+        }
+    }
+}
