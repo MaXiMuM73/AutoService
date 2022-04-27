9d16c23dd4e0ae826c8921c730c48c762b740f27
Maksim Sunoplya <969715@mail.ru>
2022-04-27 14:11:04 +0400

    Primitive variable test

---
+++ src/test/java/com/cars/multithreading/atomic/PrimitiveVariableTest.java
@@ -0,0 +1,29 @@
+package com.cars.multithreading.atomic;
+
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Log4j2
+public class PrimitiveVariableTest {
+    private static final AtomicInteger count = new AtomicInteger(0);
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            for (int i = 0; i < 100000; i++) {
+                count.incrementAndGet();
+                System.out.println(Thread.currentThread().getName() + " count = " + count);
+            }
+            System.out.println(Thread.currentThread().getName() + " Final count = " + count);
+        };
+
+        new Thread(task).start();
+        new Thread(task).start();
+        new Thread(task).start();
+
+        Thread.sleep(5000);
+        log.info("Final: {}", count);
+    }
+}
