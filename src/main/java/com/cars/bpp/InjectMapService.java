857d8c7b6574fb595eb3aac4e4493d3faec305b6
Maksim Sunoplya <969715@mail.ru>
2022-04-29 17:52:05 +0400

    Test2

---
+++ src/main/java/com/cars/bpp/InjectMapService.java
@@ -0,0 +1,26 @@
+package com.cars.bpp;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
+
+@Log4j2
+@Service(value = "InjectMapService")
+@Transactional
+public class InjectMapService {
+
+    @InjectMap
+    private Map<String, MyCustomService> map;
+
+    public InjectMapService() {
+        log.info("Constructor");
+    }
+
+    @PostConstruct
+    public void initMethod() {
+        log.info("Post construct");
+    }
+}
