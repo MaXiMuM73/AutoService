36f4a7ab8aaee9e9ed3aa9914c6eea6463213087
Maksim Sunoplya <969715@mail.ru>
2022-05-08 21:55:47 +0400

    Set add all method test

---
+++ src/test/java/com/cars/collection/SetTest.java
@@ -0,0 +1,24 @@
+package com.cars.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+
+    @Test
+    void testAddAll() {
+        Set<Long> ids = new HashSet<>();
+        Set<Long> ids2 = new HashSet<>();
+        ids.add(1L);
+        ids.add(2L);
+        ids.add(4L);
+        ids2.add(1L);
+        ids2.add(2L);
+        ids2.add(3L);
+        boolean b = ids.addAll(ids2);
+        System.out.println(b);
+        System.out.println(ids);
+    }
+}
