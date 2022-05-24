e5b9ee69f13cb2ba4b0faf8b3664ad2c077d1f13
Maksim Sunoplya <969715@mail.ru>
2022-05-24 10:40:08 +0400

    Added generic test

---
+++ src/test/java/com/cars/generics/GenericTest.java
@@ -0,0 +1,31 @@
+package com.cars.generics;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericTest {
+
+    public static void main(String[] args) {
+        String testString = "TestString";
+        String test = getT(testString);
+        System.out.println("Test string: " + test);
+
+        Integer number = 55;
+        Integer t = getT(number);
+        System.out.println("Number: " + t);
+    }
+
+    private static <T> T getT(T t) {
+        return (T) t;
+    }
+
+    @Test
+    public void checkRawTypes() {
+        List list = new ArrayList();
+        list.add("Test");
+        list.add(123);
+        System.out.println(list);
+    }
+}
