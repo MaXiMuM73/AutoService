6e1ccdda57f366ed882ab6ff2f1d855dc308b8f0
Maksim Sunoplya <969715@mail.ru>
2022-11-09 09:06:25 +0400

    Added logging

--- src/test/java/com/cars/java15/TextBlockTest.java
+++ src/test/java/com/cars/java15/TextBlockTest.java
@@ -5,12 +5,12 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class TextBlockTest {
     public static void main(String[] args) {
-        String TEXT_BLOCK_JSON = """
-                {
-                    "name" : "someName",
-                    "site" : "https://www.someSite.com/"
-                }
-                """;
-        log.info(TEXT_BLOCK_JSON);
+//        String TEXT_BLOCK_JSON = """
+//                {
+//                    "name" : "someName",
+//                    "site" : "https://www.someSite.com/"
+//                }
+//                """;
+//        log.info(TEXT_BLOCK_JSON);
     }
 }
